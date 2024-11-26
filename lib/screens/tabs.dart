d47f4b9f9fbc41873f3b9f2f0b82c6db403b6602
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-11-27 00:24:43 +0700

    Replacing Screens (Instead of Pushing)

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
@@ -47,10 +48,13 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
     if (identifier == 'filters') {
-      _selectPage(0);
-    } else {
-      Navigator.pop(context);
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
     }
   }
 
