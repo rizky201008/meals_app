0de774669a8c066c258d7931fd40817e2a103cd5
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-11-26 12:47:05 +0700

    closing the drawer manually

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -46,6 +46,14 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == 'filters') {
+      _selectPage(0);
+    } else {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -65,7 +73,9 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
