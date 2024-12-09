78953ae344d3f7dd3ce1c8544e4c6da48bc199ed
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-12-09 11:57:38 +0700

    Configuring Implicit Animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -36,7 +36,22 @@ class MealDetails extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(
+                    begin: 0.5,
+                    end: 1.0,
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ],
       ),
